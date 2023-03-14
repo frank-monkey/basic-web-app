d32db3bbaa968cca78c17946daff3bb50c64ae9b
Frank <fsacco@andrew.cmu.edu>
2023-03-14 16:19:11 -0400

    aaa

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -18,15 +18,31 @@ export default function QueryProcessor(query: string): string {
     );
   }
   if (query.toLowerCase().includes("largest")) {
-  return findLargestNumber(query).toString();
+  return C(query).toString();
+  }
+  if (query.toLowerCase().includes("both a square and cube")) {
+  return D(query).toString();
   }
   else
     return "0";
 }
 
-function findLargestNumber(str: string): number {
-  const regex = /\d+/g; // Matches any digit one or more times
-  const numbers = str.match(regex)?.map(Number); // Extract all numbers from the string and convert them to numbers
-  if (!numbers || numbers.length === 0) return -1; // If no numbers are found in the string, return -1
-  return Math.max(...numbers); // Return the largest number
+function C(str: string): number {
+  const regex = /\d+/g;
+  const numbers = str.match(regex)?.map(Number);
+  if (!numbers || numbers.length === 0) return -1;
+  return Math.max(...numbers);
+}
+
+function D(str: string) : number {
+  const regex = /\d+/g;
+  const numbers = str.match(regex)?.map(Number);
+  if (!numbers || numbers.length === 0) return -1;
+  for(let i = 0; i < numbers.length; i++) {
+    const a = (Math.pow(Math.round(Math.sqrt(numbers[i])), 2) == numbers[i]);
+    const b = (Math.pow(Math.round(Math.cbrt(numbers[i])), 3) == numbers[i]);
+    if(a && b)
+      return numbers[i];
+  }
+  return -1;
 }
